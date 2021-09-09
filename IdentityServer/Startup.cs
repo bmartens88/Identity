e404b1b2aad4c9bb295f632a36c2237b62c1c729
Bas Martens <bmartens1988@kpnmail.nl>
2021-09-09 16:52:22 +0200

    Added EF migrations. Stopped at 21:08 mark

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,8 +1,10 @@
+
 using IdentityServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,11 +12,20 @@ namespace IdentityServer
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(config => { config.UseInMemoryDatabase("Memory"); });
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddDbContext<AppDbContext>(config => config.UseSqlite(connectionString));
 
             services.AddIdentity<IdentityUser, IdentityRole>(config =>
                 {
@@ -32,13 +43,25 @@ namespace IdentityServer
                 config.LoginPath = "/Auth/Login";
             });
 
+            var assembly = typeof(Startup).Assembly.GetName().Name;
+
             services
                 .AddIdentityServer()
                 .AddAspNetIdentity<IdentityUser>()
-                .AddInMemoryIdentityResources(Configuration.GetResources())
-                .AddInMemoryApiResources(Configuration.GetApis())
-                .AddInMemoryClients(Configuration.GetClients())
-                .AddInMemoryApiScopes(Configuration.GetScopes())
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                        sql => sql.MigrationsAssembly(assembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                        sql => sql.MigrationsAssembly(assembly));
+                })
+                // .AddInMemoryIdentityResources(Configuration.GetResources())
+                // .AddInMemoryApiResources(Configuration.GetApis())
+                // .AddInMemoryClients(Configuration.GetClients())
+                // .AddInMemoryApiScopes(Configuration.GetScopes())
                 .AddDeveloperSigningCredential();
 
             services.AddControllersWithViews();
